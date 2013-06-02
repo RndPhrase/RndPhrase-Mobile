1a9a41d9b7406455c9737c824ee3ebebf05fb378
Ronni Elken Lindsgaard <rel@it-minds.dk>
2013-06-02 02:06:50 +0200

    Added eventhandler for textfields

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,1 +1,8 @@
+function doRndphrase(e){
+	//If nothing is there, it does not make sense to rndphrase
+	if($.seed.value != '' && $.password.value != '' && $.domain.value != ''){
+		$.rndphrase.value = $.seed.value + $.password.value + $.domain.value;
+	}
+}
+
 $.index.open();
