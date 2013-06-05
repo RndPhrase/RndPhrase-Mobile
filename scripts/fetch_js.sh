c3bf877d5133897f23a0f6632132e4ee086463b1
Ronni Elken Lindsgaard <rel@it-minds.dk>
2013-06-05 17:41:24 +0200

    Changed the fetch_js script a bit

--- scripts/fetch_js.sh
+++ scripts/fetch_js.sh
@@ -2,11 +2,19 @@
 RNDPHRASE=https://raw.github.com/brinchj/RndPhrase/master/
 OUTDIR=${1}/
 CMD="/usr/bin/curl -L"
+HASH="^#"
+HASH_REPLACE="\/\/#"
 SEARCH="^#include \\(\"\\)\\(.*\\)\/\\(.*\\)$"
 REPLACE="require(\1\3)"
 ${CMD} ${RNDPHRASE}data/suffix-list.js > ${OUTDIR}suffix-list.js
-
-${CMD} ${RNDPHRASE}lib/common.js > ${OUTDIR}common.js
-${CMD} ${RNDPHRASE}lib/cubehash.js | sed "s/${SEARCH}/${REPLACE}/"> ${OUTDIR}cubehash.js
-${CMD} ${RNDPHRASE}lib/domainmanager.js | sed "s/${SEARCH}/${REPLACE}/" > ${OUTDIR}domainmanager.js
-${CMD} ${RNDPHRASE}lib/rndphrase.js | sed "s/${SEARCH}/${REPLACE}/" > ${OUTDIR}rndphrase.js
+${CMD} ${RNDPHRASE}lib/common.js \
+| sed "s/${HASH}/${HASH_REPLACE}/" > ${OUTDIR}common.js
+${CMD} ${RNDPHRASE}lib/cubehash.js \
+| sed "s/${SEARCH}/${REPLACE}/" \
+| sed "s/${HASH}/${HASH_REPLACE}/" > ${OUTDIR}cubehash.js
+${CMD} ${RNDPHRASE}lib/domainmanager.js \
+| sed "s/${SEARCH}/${REPLACE}/" \
+| sed "s/${HASH}/${HASH_REPLACE}/" > ${OUTDIR}domainmanager.js
+${CMD} ${RNDPHRASE}lib/rndphrase.js \
+| sed "s/${SEARCH}/${REPLACE}/" \
+| sed "s/${HASH}/${HASH_REPLACE}/" > ${OUTDIR}rndphrase.js
